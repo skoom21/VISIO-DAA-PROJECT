e1b43607cb5e0590e1f92bf6fd60f64795a9d19a
Talha Yousif <k225146@nu.edu.pk>
2024-11-21 03:12:08 +0500

    i wanna sleep now

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/my-app', // Set the sub-path for deployment
-    reactStrictMode: true, // Optional: Enable React strict mode for better debugging
   };
   
   export default nextConfig;
