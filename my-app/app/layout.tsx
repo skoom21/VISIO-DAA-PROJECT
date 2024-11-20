e3fe321da12aff00ba0549fe11312a66d48baec9
Talha Yousif <k225146@nu.edu.pk>
2024-11-21 02:33:47 +0500

    final

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/visio-logo.svg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
