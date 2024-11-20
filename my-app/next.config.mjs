36ca495900f999c3f67a3f3a848f7199b84c85e2
Talha Yousif <k225146@nu.edu.pk>
2024-11-21 03:05:24 +0500

    final fix please god

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -2,9 +2,6 @@
 const nextConfig = {
     basePath: '/my-app', // Set the sub-path for deployment
     reactStrictMode: true, // Optional: Enable React strict mode for better debugging
-    images: {
-      unoptimized: true, // If you're not using Next.js image optimization
-    },
   };
   
   export default nextConfig;
